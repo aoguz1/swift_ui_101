9de5f1e66b50bbf81a6e8d8f052fc52c73ed726a
Abdullah Oğuz <abdullah.oguz@mdpgroup.com>
2021-09-08 19:09:22 +0300

    Initial Commit

---
+++ swift_ui_101/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift_ui_101
+//
+//  Created by Abdullah Oğuz on 8.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_ui_101/swift_ui_101App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_101App.swift
+//  swift_ui_101
+//
+//  Created by Abdullah Oğuz on 8.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_101App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
